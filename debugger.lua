3aad3b31f1b46af360ad7599ee8cee7bfc51ec1e
haroldjd2017ipad <60534549+haroldjd2017ipad@users.noreply.github.com>
2024-06-28 14:39:35 +0800

    Update debugger.lua

--- debugger.lua
+++ debugger.lua
@@ -1,31 +1,31 @@
+-- loadstring(game:HttpGet("https://raw.githubusercontent.com/haroldjd2017ipad/roblox/main/debugger.lua", true))
+
 if game.CoreGui:FindFirstChild("Debugger") then
 	game.CoreGui.Debugger:Destroy()
 end
 
+local Debugger = Instance.new("ScreenGui", game.CoreGui)
+local Frame = Instance.new("Frame", Debugger)
+local UIListLayout = Instance.new("UIListLayout", Frame)
+
+Debugger.Name = "Debugger"
+Debugger.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Frame.AnchorPoint = Vector2.new(0, 1)
+Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame.BackgroundTransparency = 0.500
+Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Frame.BorderSizePixel = 0
+Frame.Position = UDim2.new(0.82426405, 0, 1, 0)
+Frame.Size = UDim2.new(0, 197, 0, 0)
+Frame.AutomaticSize = Enum.AutomaticSize.Y
+
+UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
+
 debugger = {
-	createUI = function()
-		local Debugger = Instance.new("ScreenGui", game.CoreGui)
-		local Frame = Instance.new("Frame", Debugger)
-		local UIListLayout = Instance.new("UIListLayout", Frame)
-
-		Debugger.Name = "Debugger"
-		Debugger.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-
-		Frame.AnchorPoint = Vector2.new(0, 1)
-		Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-		Frame.BackgroundTransparency = 0.500
-		Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
-		Frame.BorderSizePixel = 0
-		Frame.Position = UDim2.new(0.82426405, 0, 1, 0)
-		Frame.Size = UDim2.new(0, 197, 0, 0)
-		Frame.AutomaticSize = Enum.AutomaticSize.Y
-
-		UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
-	end,
-	
 	update = function(label, text)
-		if not game.CoreGui.Debugger.Frame:FindFirstChild(label) then
-			local TextLabel = Instance.new("TextLabel", game.CoreGui.Debugger.Frame)
+		if not Frame:FindFirstChild(label) then
+			local TextLabel = Instance.new("TextLabel", Frame)
 			TextLabel.Name = label
 			TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 			TextLabel.BackgroundTransparency = 1.000
@@ -44,7 +44,7 @@ debugger = {
 	end,
 	
 	clear = function()
-		for i,v in pairs(game.CoreGui.Debugger.Frame:GetChildren()) do
+		for i,v in pairs(Frame:GetChildren()) do
 			if v:IsA("TextLabel") then
 				debugger.update(v.Name, "no info")
 			end
